fd0b5a151f592ce6abff22ae25827840eac34c2f
nityabhaskar21 <nityabhaskar21@gmail.com>
2020-05-10 12:07:44 +0530

    logger modified

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,13 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
 export default { store, persistStore };
+
